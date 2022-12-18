2d704bf3a656d10381f76482bc5cf4183c3a7e6d
vijaya2003 <20b01a1236@svecw.edu.in>
2022-12-18 16:14:17 +0530

    Add lint staged

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,17 +1,75 @@
 /* eslint-disable no-undef */
-const todoList = require("../todo");
+// const todoList = require("../todo");
 
-const { all, markAsComplete, add } = todoList();
+// const { all, markAsComplete, add } = todoList();
+
+// describe("Todolist Test Suite", () => {
+//   beforeAll(() => {
+//     add({
+//       title: "Test todo",
+//       completed: false,
+//       dueDate: new Date().toLocaleDateString("en-CA"),
+//     });
+//   });
+//   test("Should add new todo", () => {
+//     const todoItemsCount = all.length;
+//     add({
+//       title: "Test todo",
+//       completed: false,
+//       dueDate: new Date().toLocaleDateString("en-CA"),
+//     });
+//     expect(all.length).toBe(todoItemsCount + 1);
+//   });
+//   test("Should mark a todo as Complete", () => {
+//     expect(all[0].completed).toBe(false);
+//     markAsComplete(0);
+//     expect(all[0].completed).toBe(true);
+//   });
+//   test("Return a list of dueToday items", () => {
+//     var c = dueToday();
+//     expect(all[1].title).toBe(c[0].title);
+//     expect(all[1].dueDate).toBe(c[0].dueDate);
+//   });
+//   test("Return a list of overdue items", () => {
+//     var c = overdue();
+//     expect(all[2].title).toBe(c[0].title);
+//     expect(all[2].dueDate).toBe(c[0].dueDate);
+//   });
+//   test("Return a list of dueLater items", () => {
+//     var c = dueLater();
+//     expect(all[3].title).toBe(c[0].title);
+//     expect(all[3].dueDate).toBe(c[0].dueDate);
+//   });
+// });
+
+const todoList = require("../todo");
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
 describe("Todolist Test Suite", () => {
   beforeAll(() => {
+    var d = new Date();
+    d.setDate(d.getDate() - 1);
+    var i = d.toLocaleDateString("en-CA");
+    var d1 = new Date();
+    d1.setDate(d1.getDate() + 1);
+    var j = d1.toLocaleDateString("en-CA");
     add({
       title: "Test todo",
       completed: false,
       dueDate: new Date().toLocaleDateString("en-CA"),
     });
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: i,
+    });
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: j,
+    });
   });
-  test("Should add new todo", () => {
+  test("Add new todo", () => {
     const todoItemsCount = all.length;
     add({
       title: "Test todo",
@@ -20,9 +78,24 @@ describe("Todolist Test Suite", () => {
     });
     expect(all.length).toBe(todoItemsCount + 1);
   });
-  test("Should mark a todo as Complete", () => {
+  test("Mark a todo as completed", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+  test("Return a list of dueToday items", () => {
+    var c = dueToday();
+    expect(all[0].title).toBe(c[0].title);
+    expect(all[0].dueDate).toBe(c[0].dueDate);
+  });
+  test("Return a list of overdue items", () => {
+    var c = overdue();
+    expect(all[1].title).toBe(c[0].title);
+    expect(all[1].dueDate).toBe(c[0].dueDate);
+  });
+  test("Return a list of dueLater items", () => {
+    var c = dueLater();
+    expect(all[2].title).toBe(c[0].title);
+    expect(all[2].dueDate).toBe(c[0].dueDate);
   });
 });
--- todo.js
+++ todo.js
@@ -1,62 +1,4 @@
-// const http = require("http");
-// const fs = require("fs");
-
-// let homeContent = "";
-// let projectContent = "";
-// let registrationContent = "";
-// let args = require("minimist")(process.argv.slice(2));
-
-// let port = args["port"];
-
-// fs.readFile("home.html", (err, home) => {
-//   if (err) {
-//     throw err;
-//   }
-//   homeContent = home;
-// });
-
-// fs.readFile("project.html", (err, project) => {
-//   if (err) {
-//     throw err;
-//   }
-//   projectContent = project;
-// });
-
-// fs.readFile("registration.html", (err, registration) => {
-//   if (err) {
-//     throw err;
-//   }
-//   registrationContent = registration;
-// });
-
-// fs.readFile("home.html", (err, home) => {
-//   if (err) {
-//     throw err;
-//   }
-//   http
-//     .createServer((request, response) => {
-//       let url = request.url;
-//       response.writeHeader(200, { "Content-Type": "text/html" });
-//       switch (url) {
-//         case "/project":
-//           response.write(projectContent);
-//           response.end();
-//           break;
-//         case "/project/registration":
-//           response.write(registrationContent);
-//           response.end();
-//           break;
-//         default:
-//           response.write(homeContent);
-//           response.end();
-//           break;
-//       }
-//     })
-//     .listen(port);
-// });
-
 /* eslint-disable no-undef */
-
 const todoList = () => {
   all = [];
   const add = (todoItem) => {
@@ -139,49 +81,9 @@ const todoList = () => {
     toDisplayableList,
   };
 };
-module.exports = todoList;
 
 // ####################################### #
 // DO NOT CHANGE ANYTHING BELOW THIS LINE. #
 // ####################################### #
 
-// const todos = todoList();
-
-// const formattedDate = d => {
-//   return d.toISOString().split("T")[0]
-// }
-
-// var dateToday = new Date()
-// const today = formattedDate(dateToday)
-// const yesterday = formattedDate(
-//   new Date(new Date().setDate(dateToday.getDate() - 1))
-// )
-// const tomorrow = formattedDate(
-//   new Date(new Date().setDate(dateToday.getDate() + 1))
-// )
-
-// todos.add({ title: 'Submit assignment', dueDate: yesterday, completed: false })
-// todos.add({ title: 'Pay rent', dueDate: today, completed: true })
-// todos.add({ title: 'Service Vehicle', dueDate: today, completed: false })
-// todos.add({ title: 'File taxes', dueDate: tomorrow, completed: false })
-// todos.add({ title: 'Pay electric bill', dueDate: tomorrow, completed: false })
-
-// console.log("My Todo-list\n\n")
-
-// console.log("Overdue")
-// var overdues = todos.overdue()
-// var formattedOverdues = todos.toDisplayableList(overdues)
-// console.log(formattedOverdues)
-// console.log("\n\n")
-
-// console.log("Due Today")
-// let itemsDueToday = todos.dueToday()
-// let formattedItemsDueToday = todos.toDisplayableList(itemsDueToday)
-// console.log(formattedItemsDueToday)
-// console.log("\n\n")
-
-// console.log("Due Later")
-// let itemsDueLater = todos.dueLater()
-// let formattedItemsDueLater = todos.toDisplayableList(itemsDueLater)
-// console.log(formattedItemsDueLater)
-// console.log("\n\n")
+module.exports = todoList;
